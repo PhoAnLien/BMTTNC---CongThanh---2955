ac6aeae89a192231041100794b6873c2f0200b87
PhoAnLien <thanhpham.apr@gmail.com>
2025-05-20 15:58:30 +0700

    Update api.py

--- lab02/ex01/api.py
+++ lab02/ex01/api.py
@@ -10,7 +10,7 @@ def caesar_encrypt():
     plain_text = data['plain_text']
     key = int(data['key'])
     encrypted_text = caesar_cipher.encrypted_text(plain_text, key)
-    return jsonify({'encrypted_message':encrypted_text})
+    return jsonify({'encrypted_message':encrypt_text})
 
 @app.route("/api/caesar/decrypt", methods=["POST"])
 def caesar_decrypt():
@@ -18,7 +18,7 @@ def caesar_decrypt():
     cipher_text = data['cipher_text']
     key = int(data['key'])
     decrypted_text = caesar_cipher.decrypted_text(cipher_text, key)
-    return jsonify({'encrypted_message':decrypted_text})
+    return jsonify({'encrypted_message':decrypt_text})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000, debug=True)
